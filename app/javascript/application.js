a0d691ba1d0c57f53bb46fe454b24c70ab5803fe
Eric James <eric.james@yale.edu>
2023-11-10 22:20:47 -0500

    POC jquery with import maps

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "jquery"
 import bootstrap from "bootstrap"
 import githubAutoCompleteElement from "@github/auto-complete-element"
 import Blacklight from "blacklight"
--- config/importmap.rb
+++ config/importmap.rb
@@ -8,4 +8,5 @@ pin_all_from "app/javascript/controllers", under: "controllers"
 pin "@github/auto-complete-element", to: "https://cdn.skypack.dev/@github/auto-complete-element"
 pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.6/dist/umd/popper.min.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.2.2/dist/js/bootstrap.js"
-pin "jquery" # @3.7.1
+#pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.7.1/dist/jquery.js"
+pin "jquery", to: "https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.js"
